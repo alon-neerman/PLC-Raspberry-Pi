7af3d8da312623e92683feec4cad29c3c5ae25ec
Aakash Rohra <rohra.a@northeastern.edu>
2020-11-29 22:10:24 +0400

    Added PLC code to move robot

--- robot_code/move_robot.py
+++ robot_code/move_robot.py
@@ -1,100 +1,111 @@
-import RPi.GPIO as GPIO          
+#import RPi.GPIO as GPIO          
 from time import sleep
-#need to change these based on Rasp PI connections
-in1 = 23
-in2 = 24
-ena = 25
+import sys
+sys.path.append('../sensors')
+from IMU import IMU
+sys.path.append('../')
+from PLC import PLC
 
-in3 = 12
-in4 = 13
-enb = 15
-
-
-#Motor 1 assuming right motor
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1,GPIO.OUT)
-GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(ena,GPIO.OUT)
-GPIO.setup(in3,GPIO.OUT)
-GPIO.setup(in4,GPIO.OUT)
-GPIO.setup(enb,GPIO.OUT)
-#Starting at low, motor is stopped
-GPIO.output(in1,GPIO.LOW)
-GPIO.output(in2,GPIO.LOW)
-
-#Motor 2, assuming left
-
-#Starting at low, motor is stopped
-GPIO.output(in3,GPIO.LOW)
-GPIO.output(in4,GPIO.LOW)
-
-frequency = 1000;
-p=GPIO.PWM(ena,frequency)
-p2= GPIO.PWM(enb,frequency)
-#default duty cycle 25
-duty_cycle = 25
-#Steps size for increasing duty cycle
-step_size = 5
-p.start(duty_cycle)
-p2.start(duty_cycle)
-print("Default speed is dc 25.")
-print("Options: ")
-print("\n")    
-print("F to move forward\nB to move backwards\nL to move left\nR to move right\nS to stop\nI to increase speed\nD to decrease speed\nE to exit")
-while(1):
-
-    x=input()
+# Not sure how to implement PWM frequency.
+class move_robot:
+    #initialize
+    # Speed is not absolute, its a value between 0-100 where 1000 is the max speed 
+    # Threshold is for rotation accuracy, ie, how close to the desired angle are we allowed to reach
+    def __init__(self, client, in1, in2, ena_pwm, in3, in4, enb_pwm, speed, IMU, threshold):
+        self.in1 = in1
+        self.in2 = in2
+        self.ena_pwm = ena_pwm
+        self.in3 = in3
+        self.in4 = in4
+        self.enb_pwm = enb_pwm
+        self.speed = speed
+        self.client = client
+        self.IMU = IMU
+        self.threshold = threshold
+        self.start_heading = self.IMU.getHeading()
+        #set in1-in4 to low. Robot is stopped by default
+        self.client.write_digital_output(in1, 0)
+        self.client.write_digital_output(in2, 0)
+        self.client.write_digital_output(in3, 0)
+        self.client.write_digital_output(in4, 0)
+        #set speed to 0
+        self.client.write_analog_output(ena_pwm, 0)
+        self.client.write_analog_output(enb_pwm, 0)
+        #io_value is 0 or 1 
+        #client.write_digital_output(int(IO_Port), IO_Value)
+        #client.write_analog_output()
     
-    if x=='f':
-        print("Move Forward")
-        GPIO.output(in1,GPIO.HIGH)
-        GPIO.output(in3,GPIO.HIGH)
-        GPIO.output(in2,GPIO.LOW)
-        GPIO.output(in4,GPIO.LOW)
 
-    elif x=='b':
-        print("Move Backward")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in3,GPIO.LOW)
-        GPIO.output(in2,GPIO.High)
-        GPIO.output(in4,GPIO.High)
- 
-    elif x=='l':
-    	#Move right motor forward and stop left motor
-        print("Moving Left")
-        GPIO.output(in1,GPIO.HIGH)
-        GPIO.output(in3,GPIO.LOW)
-        GPIO.output(in2,GPIO.LOW)
-        GPIO.output(in4,GPIO.LOW)
+    #Start robot motion.
+    # If forward is 1 robot moves forward. If its 0, robot moves backward
+    def startMotion(self, forward):
+        #Robot moving forward
+        if forward==1:
+            #set in1 and in3 to High
+            self.client.write_digital_output(in1, 1)
+            self.client.write_digital_output(in3, 1)
+            #Set in2 and in4 to low
+            self.client.write_digital_output(in2, 0)
+            self.client.write_digital_output(in4, 0)
+            self.client.write_analog_output(ena_pwm, speed)
+            self.client.write_analog_output(enb_pwm, speed)
+        #Robot moving backward
+        elif forward==0:
+            #set in1 and in3 to Low
+            self.client.write_digital_output(in1, 0)
+            self.client.write_digital_output(in3, 0)
+            #Set in2 and in4 to High
+            self.client.write_digital_output(in2, 1)
+            self.client.write_digital_output(in4, 1)
+            self.client.write_analog_output(ena_pwm, speed)
+            self.client.write_analog_output(enb_pwm, speed)
 
-    elif x=='r':
-    	#Moving left motor forward but stop the right
-        print("Moving right")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in3,GPIO.HIGH)
-        GPIO.output(in2,GPIO.LOW)
-        GPIO.output(in4,GPIO.LOW)
-  
-    elif x=='s':
-        print("Stopping the robot")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in3,GPIO.LOW)
-        GPIO.output(in2,GPIO.LOW)
-        GPIO.output(in4,GPIO.LOW)
-   
+    #Stops robot motion
+    def stopMotion(self):
+        #set in1-in4 to low
+        self.client.write_digital_output(in1, 0)
+        self.client.write_digital_output(in2, 0)
+        self.client.write_digital_output(in3, 0)
+        self.client.write_digital_output(in4, 0)
+        #set speed to 0 as well
+        self.client.write_analog_output(ena_pwm, 0)
+        self.client.write_analog_output(enb_pwm, 0)
 
-    elif x=='i':
-        print("Increasing speed")
-        duty_cylcle = duty_cycle + step_size
-        p.ChangeDutyCycle(duty_cycle)
-	p2.ChangeDutyCycle(duty_cycle)
+    #Rotate robot, angle in degrees. Assume that starting angle is 0.
+    def rotateRobot(self, angle):
+        #angle is starting heading + desired angle
+        rotate_angle = angle + self.start_heading
+        #move right to increase angle and left to decrease
+        if (rotate_angle > start_heading):
+            angle_reached = False
+            while(angle_reached != True):
+                #keep rotating right motor
+                #Set in3 to true. Move only left motor forward and stop right motor
+                self.client.write_digital_output(in1, 0)
+                self.client.write_digital_output(in2, 0)
+                self.client.write_digital_output(in3, 1)
+                self.client.write_digital_output(in4, 0)
+                self.client.write_analog_output(ena_pwm, 0)
+                self.client.write_analog_output(enb_pwm, self.speed)
+                if (-threshold < (self.IMU.getHeading() - angle) < threshold):
+                    angle_reached = True
 
-    elif x=='d':
-        print("Decreasing speed")
-        duty_cylcle = duty_cycle - step_size
-        p.ChangeDutyCycle(duty_cycle)
-    	p2.ChangeDutyCycle(duty_cycle)
-    elif x=='e':
-        print("exitting")
-        GPIO.cleanup()
-        break
+        elif (rotate_angle < start_heading):
+            angle_reached = False
+            while(angle_reached != True):
+                #keep rotating left
+                #Set in1 to true. Move only right motor forward and stop left motor
+                self.client.write_digital_output(in1, 1)
+                self.client.write_digital_output(in2, 0)
+                self.client.write_digital_output(in3, 0)
+                self.client.write_digital_output(in4, 0)
+                self.client.write_analog_output(ena_pwm, self.speed)
+                self.client.write_analog_output(enb_pwm, 0)
+                if (-threshold <= (self.IMU.getHeading() - angle) <= threshold):
+                    angle_reached = True
+
+    #Changes speed. Speed is not absolute, need value from 0 - 1000
+    def changeSpeed(self, speed):
+        self.speed = speed
+        self.client.write_analog_output(ena_pwm, self.speed)
+        self.client.write_analog_output(enb_pwm, self.speed)
